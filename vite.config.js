397d101246ad0caa1c87eb56562be425eee07f3f
Arfaz Ahmed <arfazaar14@gmail.com>
2025-05-12 13:58:33 +0530

    base added to config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import federation from '@originjs/vite-plugin-federation'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/login-app/',
   plugins: [react(),
     federation({
       name:'loginApp',
